c0ef6db23d2b806ec95f9493f147447071bc7971
Yash <yash@khemani.com>
2020-06-23 15:20:54 +0000

    update for terraform 0.13

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "terraform-providers/digitalocean"
+    }
+  }
+}
+
 resource digitalocean_ssh_key ssh_key {
   name       = var.ssh_key_name
   public_key = var.ssh_public_key
